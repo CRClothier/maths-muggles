38984ed117484eb287e1be8b57cba70db8bf4c7d
CRClothier <c.r.clothier@gmail.com>
2022-03-24 20:49:16 -0500

    Refactor all classes as stateless funtions

--- src/components/CalculatorPage.js
+++ src/components/CalculatorPage.js
@@ -1,16 +1,12 @@
-/* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
 import Calculator from './Calculator';
 
-class CalculatorPage extends Component {
-  render() {
-    return (
-      <div className="calculator-main">
-        <h2>Lets do some maths!!</h2>
-        <Calculator />
-      </div>
-    );
-  }
+function CalculatorPage() {
+  return (
+    <div className="calculator-main">
+      <h2>Lets do some maths!!</h2>
+      <Calculator />
+    </div>
+  );
 }
 
 export default CalculatorPage;
--- src/components/QuotePage.js
+++ src/components/QuotePage.js
@@ -1,18 +1,13 @@
-/* eslint-disable react/prefer-stateless-function */
-import { Component } from 'react';
-
-class QuotePage extends Component {
-  render() {
-    return (
-      <div>
-        <h2 className="quote">
-          Mathematics is not about numbers,
-          equations, computations, or algorithms: it is about
-          understanding. -William Paul Thurston
-        </h2>
-      </div>
-    );
-  }
+function QuotePage() {
+  return (
+    <div>
+      <h2 className="quote">
+        Mathematics is not about numbers,
+        equations, computations, or algorithms: it is about
+        understanding. -William Paul Thurston
+      </h2>
+    </div>
+  );
 }
 
 export default QuotePage;
